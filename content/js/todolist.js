87aa86461aa2a05a8e941c40f6d1bb8bd37111e6
rajat sardesai <rajatsardesai@gmail.com>
2023-04-03 23:07:44 +0530

    feat: add todos list app

---
+++ content/js/todolist.js
@@ -0,0 +1,63 @@
+/* Code for to-do list app */
+
+const todoItems = document.querySelector('#todos');
+const todoBox = document.querySelector('#todo-box');
+
+// Add todos on Enter
+todoItems.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        if (todoItems.value != "") {
+            addTodo(todoItems.value);
+            todoItems.value = "";
+        }
+    }
+});
+
+// Function to save todos to localstorage
+const saveTodos = () => {
+    const todos = document.querySelectorAll(".list-group-item");
+    const data = [];
+    todos.forEach(todo => {
+        data.push(todo.innerText);
+    });
+    if (data.length === 0) {
+        localStorage.removeItem("todos");
+    } else {
+        localStorage.setItem("todos", JSON.stringify(data));
+    }
+}
+
+// Function to add todos
+const addTodo = (todo) => {
+    const listItem = document.createElement("li");
+    const classLists = ["list-group-item", "d-flex", "justify-content-between", "align-items-start", "font-xs-18"];
+    classLists.forEach(classes => listItem.classList.add(classes));
+    listItem.innerHTML = `
+        ${todo}
+        <i class="delete fa-solid fa-times fa-lg text-dark p-3 cursor-pointer"></i>`;
+    todoBox.appendChild(listItem);
+
+    // Code to mark todo as done
+    listItem.addEventListener('click', () => {
+        listItem.classList.toggle("text-decoration-line-through");
+        saveTodos();
+    });
+
+    // Code to delete todo
+    listItem.querySelector('.delete').addEventListener('click', () => {
+        listItem.remove();
+        saveTodos();
+    });
+
+    saveTodos();
+};
+
+// Initial fuction on load to get item from localstorage
+(
+    function () {
+        const lsTodos = JSON.parse(localStorage.getItem("todos"));
+        if (lsTodos !== null) {
+            lsTodos.forEach(todo => addTodo(todo));
+        }
+    }
+)()
